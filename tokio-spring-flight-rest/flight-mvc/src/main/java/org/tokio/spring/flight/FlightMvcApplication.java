c0b2568a19a7bb069f46774258a3b4555f398d3a
andres.rpenuela <andresruizpenuela@gmail.com>
2024-11-26 19:17:10 +0100

    [PRJ00008] se crea el modulo de flight mvc con la configuración básica

---
+++ tokio-spring-flight-rest/flight-mvc/src/main/java/org/tokio/spring/flight/FlightMvcApplication.java
@@ -0,0 +1,11 @@
+package org.tokio.spring.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlightMvcApplication.class, args);
+    }
+}
